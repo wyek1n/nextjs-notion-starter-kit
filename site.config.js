9c5760a6125133339511d19b8ed72daa73546a9a
wyek1n <96416488+wyek1n@users.noreply.github.com>
2022-04-09 08:33:24 +0800

    Add files via upload

---
+++ site.config.js
@@ -0,0 +1,45 @@
+export default {
+  // where it all starts -- the site's root Notion page (required)
+  rootNotionPageId: 'c98e8b62021c4c7a95bc7b861cbab2c8',
+
+  // if you want to restrict pages to a single notion workspace (optional)
+  // (this should be a Notion ID; see the docs for how to extract this)
+  rootNotionSpaceId: null,
+
+  // basic site info (required)
+  name: 'admin@wye.cool',
+  domain: 'wye.cool',
+  author: 'wyek1n',
+
+  // open graph metadata (optional)
+  description: 'A navigation for wyek1n.',
+  socialImageTitle: 'A navigation for wyek1n.',
+  socialImageSubtitle: 'Hi there! 👋',
+
+  // social usernames (optional)
+
+
+  // default notion icon and cover images for site-wide consistency (optional)
+  // page-specific values will override these site-wide defaults
+  defaultPageIcon: null,
+  defaultPageCover: null,
+  defaultPageCoverPosition: 0.5,
+
+  // whether or not to enable support for LQIP preview images (optional)
+  isPreviewImageSupportEnabled: true,
+
+  // whether or not redis is enabled for caching generated preview images (optional)
+  // NOTE: if you enable redis, you need to set the `REDIS_HOST` and `REDIS_PASSWORD`
+  // environment variables. see the readme for more info
+  isRedisEnabled: false,
+
+  // map of notion page IDs to URL paths (optional)
+  // any pages defined here will override their default URL paths
+  // example:
+  //
+  // pageUrlOverrides: {
+  //   '/foo': '067dd719a912471ea9a3ac10710e7fdf',
+  //   '/bar': '0be6efce9daf42688f65c76b89f8eb27'
+  // }
+  pageUrlOverrides: null
+}
